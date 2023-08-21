c4952e2678c544c4d60a7c0a41f5eb394255bdda
Kargisa <luca.wolf@outlook.com>
2023-08-21 12:38:58 +0200

    Initial

---
+++ src/main/java/at/porscheinformatik/desk/POIDeskAPI/PoiDeskApiApplication.java
@@ -0,0 +1,13 @@
+package at.porscheinformatik.desk.POIDeskAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoiDeskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/porscheinformatik/desk/POIDeskAPI/PoiDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.porscheinformatik.desk.POIDeskAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
