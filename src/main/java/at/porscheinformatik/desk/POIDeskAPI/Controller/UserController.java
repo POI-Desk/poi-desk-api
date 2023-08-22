e09511308c5f690a8f82980cb93451b159d1fbe1
Kargisa <luca.wolf@outlook.com>
2023-08-22 11:16:53 +0200

    user mutation

--- src/main/java/at/porscheinformatik/desk/POIDeskAPI/Controller/UserController.java
+++ src/main/java/at/porscheinformatik/desk/POIDeskAPI/Controller/UserController.java
@@ -2,10 +2,14 @@ package at.porscheinformatik.desk.POIDeskAPI.Controller;
 
 import at.porscheinformatik.desk.POIDeskAPI.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class UserController
@@ -15,4 +19,16 @@ public class UserController
 
     @QueryMapping
     public List<User> getAllUsers() { return (List<User>)userRepo.findAll(); }
+
+    @MutationMapping
+    public User changeUsername(@Argument UUID id, @Argument String name)
+    {
+        System.out.println(id);
+        if (userRepo.findById(id).isEmpty())
+            return null;
+        User user = userRepo.findById(id).get();
+        user.setUsername(name);
+        userRepo.save(user);
+        return user;
+    }
 }
