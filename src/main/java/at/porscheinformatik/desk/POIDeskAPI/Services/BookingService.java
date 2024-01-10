fab09af17388d3cbf6de44056fd4886a5ccce6a2
Kargisa <luca.wolf@outlook.com>
2024-01-10 23:07:35 +0100

    Sql changes + bookings by date and floor

--- src/main/java/at/porscheinformatik/desk/POIDeskAPI/Services/BookingService.java
+++ src/main/java/at/porscheinformatik/desk/POIDeskAPI/Services/BookingService.java
@@ -2,6 +2,7 @@ package at.porscheinformatik.desk.POIDeskAPI.Services;
 
 import at.porscheinformatik.desk.POIDeskAPI.ControllerRepos.BookingRepo;
 import at.porscheinformatik.desk.POIDeskAPI.Models.Booking;
+import at.porscheinformatik.desk.POIDeskAPI.Models.Floor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -9,12 +10,16 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class BookingService {
 
     @Autowired
     private BookingRepo bookingRepo;
+
+    @Autowired
+    private FloorService floorService;
 
     public UUID deleteBooking(UUID bookingId){
         Optional<Booking> booking = bookingRepo.findById(bookingId);
@@ -25,16 +30,17 @@ public class BookingService {
         return bookingId;
     }
 
-    public List<Booking> deleteBookings(List<UUID> bookingIds){
+    public boolean deleteBookings(List<UUID> bookingIds){
         Iterable<Booking> i_booking = bookingRepo.findAllById(bookingIds);
         bookingRepo.deleteAll(i_booking);
 
-        return (List<Booking>)i_booking;
+        return true;
     }
 
     @Async
-    public CompletableFuture<List<Booking>> getBookingsOnDate(LocalDate date){
-        Iterable<Booking> i_bookings = bookingRepo.findBookingsByDate(date);
+    public CompletableFuture<List<Booking>> getBookingsByDateOnFloor(LocalDate date, UUID floorId) throws ExecutionException, InterruptedException {
+        Floor floor = floorService.getFloorById(floorId).get();
+        Iterable<Booking> i_bookings = bookingRepo.findBookingsByDateAndDeskFloor(date, floor);
         List<Booking> bookings = new ArrayList<>();
         for (Booking booking:
              i_bookings) {
