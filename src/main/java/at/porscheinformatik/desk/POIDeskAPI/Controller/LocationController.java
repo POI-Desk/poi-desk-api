dfb8a4860c5042cd0e8318e3eaa4ff6133509b73
Sofia <0255@htl.rennweg.at>
2024-01-23 17:59:29 +0100

    implemented addLocation mutation

--- src/main/java/at/porscheinformatik/desk/POIDeskAPI/Controller/LocationController.java
+++ src/main/java/at/porscheinformatik/desk/POIDeskAPI/Controller/LocationController.java
@@ -3,6 +3,8 @@ package at.porscheinformatik.desk.POIDeskAPI.Controller;
 import at.porscheinformatik.desk.POIDeskAPI.ControllerRepos.LocationRepo;
 import at.porscheinformatik.desk.POIDeskAPI.Models.Location;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -22,4 +24,12 @@ public class LocationController {
      */
     @QueryMapping
     public List<Location> getAllLocations() { return (List<Location>)locationRepo.findAll(); }
+
+    @MutationMapping
+    public Location addLocation(@Argument String name) {
+        Location location = new Location();
+        location.setLocationname(name);
+        locationRepo.save(location);
+        return location;
+    }
 }
